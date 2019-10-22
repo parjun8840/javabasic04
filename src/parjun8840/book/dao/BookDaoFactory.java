081eba427a15be7fad7b25a1d6d4937b249fa0d8
arjun.pandey <arjun.pandey@appdynamics.com>
2019-10-22 11:49:20 +0530

    first commit

--- src/parjun8840/book/dao/BookDaoFactory.java
+++ src/parjun8840/book/dao/BookDaoFactory.java
@@ -13,10 +13,24 @@ public class BookDaoFactory {
 	}
 	// For resource bundle "." operator means package. "config" means not in any package. 
 	//For resource bundle it has to be .properties file.
+	//Programatically loading class: Class.forName(name)
+	// Creating an object using reflection api: Class.forName(name).newInstance();
+	
+	public static BookDao getBookDao() {
+		try {
+			return (BookDao) Class.forName(name).newInstance();
+			
+		}catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	/* To manually create Object of Dao
 	public static BookDao getBookDao() {
 	
 		
 		return new BookDaoMongoImpl();
 	}
-
+    */
 }
